3223e8baeef0ac3a3a311075f98a7334c2e8af97
UmmeHania <87921374+UmmeHania@users.noreply.github.com>
2022-02-17 16:03:03 +0600

    fixed some js issues

---
+++ script.js
@@ -0,0 +1,59 @@
+function getInput(inputValue) {
+    let inputCost = document.getElementById(inputValue + '-cost');
+    let inputNumber = parseFloat(inputCost.value);
+    if (inputNumber > 0 || inputNumber == 0) {
+        return inputNumber;
+    }
+    else if (inputNumber < 0) {
+        alert('Please enter a valid amount')
+    }
+    else if (isNaN(inputNumber)) {
+        alert('Please do not use any text here')
+    }
+
+}
+
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    let inputFoodCost = getInput('food');
+    let inputRentCost = getInput('rent');
+    let inputClothCost = getInput('cloth');
+    let totalCost = inputFoodCost + inputRentCost + inputClothCost;
+    let totalExpenses = document.getElementById('total-expenses');
+    totalExpenses.innerText = totalCost;
+    let incomeInput = document.getElementById('income-input');
+    let incomeTotal = parseFloat(incomeInput.value);
+    let balance = document.getElementById('balance');
+    balance.innerText = incomeTotal - totalCost;
+
+})
+document.getElementById('save-btn').addEventListener('click', function () {
+    let savingInput = document.getElementById('percentage-of-saving');
+    let saving = parseFloat(savingInput.value);
+    if (saving <= 100 || saving == 0) {
+        let savingTotal = (saving / 100) * balance.innerText;
+        let savingAmount = document.getElementById('saving-amount');
+        savingAmount.innerText = savingTotal;
+        let remainingBalance = document.getElementById('remaining-balance');
+        remainingBalance.innerText = balance.innerText - savingTotal;
+    }
+    else /*if (saving > 100 || saving < 0)*/ {
+        alert('Enter a valid percentage');
+    }
+})
+
+// document.getElementById('calculate-btn').addEventListener('click', function () {
+//     let inputfoodCost = document.getElementById('food-cost');
+//     let foodCost = parseFloat(inputfoodCost.value);
+//     let inputRentCost = document.getElementById('rent-cost');
+//     let rentCost = parseFloat(inputRentCost.value);
+//     let inputClothCost = document.getElementById('cloth-cost');
+//     let clothCost = parseFloat(inputClothCost.value);
+//     let totalCost = foodCost + rentCost + clothCost;
+//     let totalExpenses = document.getElementById('total-expenses');
+//     totalExpenses.innerText = totalCost;
+//     let incomeInput = document.getElementById('income-input');
+//     let incomeTotal = parseFloat(incomeInput.value);
+//     let balance = document.getElementById('balance');
+//     balance.innerText = incomeTotal - totalCost;
+
+// })
